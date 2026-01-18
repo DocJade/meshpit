e2af1ef7c9c1b38b0de81e0ae750c1e08c0d2fb2
DocJade <ytdocjade@gmail.com>
2026-01-17 23:19:25 -0700

    make ws global and return errors to rust

--- src/tests/startup_scripts/eval_socket.lua
+++ src/tests/startup_scripts/eval_socket.lua
@@ -1,26 +1,27 @@
+---@diagnostic disable: undefined-field
 local url = "ws:/###URL###:###SOCKET###"
-local ws, err = http.websocket(url)
+ws, err = http.websocket(url) 
+
 if not ws then
-    -- couldn't open the websocket.
-    -- Nothing we can do.
     print("Error: " .. err)
     os.shutdown()
 end
+
 while true do
     local message = ws.receive()
     if message then
         local func, loadErr = load(message, "websocket_eval")
         if func then
-            -- do a pcall so we dont crash on invalid code
             local success, runErr = pcall(func)
             if not success then
                 print("Runtime Error: " .. runErr)
+                -- also send error out the websocket so rust can see it
+                ws.send("ERROR: " .. runErr) 
             end
         else
             print("Syntax Error: " .. loadErr)
         end
     else
-        -- If message is nil, the socket closed
         print("Connection lost.")
         break
     end
